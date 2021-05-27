3a5b99aeaaa1a11f46e0d9eee96efbcf0a745ae5
García Mier <rgmier@minsait.com>
2021-05-27 16:32:16 +0200

    Feat[web-notification-message]: carpetas y ficheros básicos del componente

---
+++ src/web-notification-message.js
@@ -0,0 +1,27 @@
+import { LitElement, html } from 'lit-element';
+
+export class webNotificationMessage extends LitElement {
+
+  static get properties() {
+    return {
+ 
+    };
+  }
+
+  constructor() {
+    
+    super();
+
+  }
+
+  render() {
+    return html`
+    
+      
+    `;
+  }
+
+}
+
+
+customElements.define('web-notification-message', webNotificationMessage);
