9040e2ca7a1db03b9d5f9810aeb68fe9ebff7f99
García Mier <rgmier@minsait.com>
2021-05-27 17:00:50 +0200

    Feat[web-notification-message]: Fichero config, funcion pintar cards

--- src/config/dataMessage.js
+++ src/config/dataMessage.js
@@ -1,3 +1,33 @@
-export class configCards {
+export class configMessage {
 
+    dataCard = [
+        {
+            idCard: 'card1',
+            title: 'default',
+            subtitle: 'Information title',
+            message: 'Notification message',
+            description: 'Descripcion...',
+            typeAlert: 'primary'
+        
+        },
+
+        {
+            idCard: 'card2',
+            title: 'success',
+            subtitle: 'Information title',
+            message: 'Notification message',
+            description: 'Descripcion...',
+            typeAlert: 'success'
+        
+        },
+
+        {
+            idCard: 'card3',
+            title: 'error',
+            subtitle: 'Information title',
+            message: 'Notification message',
+            description: 'Descripcion...',
+            typeAlert: 'danger'
+        }
+    ]
 }
--- src/web-notification-message.js
+++ src/web-notification-message.js
@@ -1,23 +1,42 @@
 import { LitElement, html } from 'lit-element';
+import { configMessage } from './config/dataMessage';
 
 export class webNotificationMessage extends LitElement {
 
   static get properties() {
     return {
- 
+      dataMessage: {type: Array}
     };
   }
 
   constructor() {
-    
     super();
+    this.dataMessage = new configMessage().dataCard;
 
+  }
+
+  get paintCard() {
+    return html `
+    ${this.dataMessage.map(element => {
+      return html`
+            
+      <div class="alert alert-${element.typeAlert}">
+        This is a primary alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
+      </div>
+      
+      `
+    })}
+    `
   }
 
   render() {
     return html`
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+
+    ${this.paintCard}
     
-      
     `;
   }
 
